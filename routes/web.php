a6ad90a38248a235c7a3d32d57839fc6d8ea1e28
Ahmad Hudori <ahmadhudori06@gmail.com>
2025-08-15 19:07:56 +0700

    Add cart and checkout pages with order details and payment options

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/menu', function () {
     return view('customer.menu');
-});
+})->name('menu');
+Route::get('/cart', function () {
+    return view('customer.cart');
+})->name('cart');
+Route::get('/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
